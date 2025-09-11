8ac5193f761970144c1faf72a2e5f6446b132934
Alex-Vamper <ghostrevamper@gmail.com>
2025-09-11 19:08:50 +0100

    Update

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -305,7 +305,7 @@ export default function Settings() {
                   </g>
                 </svg>
               </div>
-              Ander - Voice Commands
+              Ander {/*- Voice Commands*/}
             </Button>
           </CardContent>
         </Card>
